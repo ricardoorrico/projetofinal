6f0780fafec734727a140b4c41ff3a0f7d335bb2
Ricardo Aparecido Orrico <39918639+ricardoorrico@users.noreply.github.com>
2025-08-30 16:56:00 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 async function fetchProfileData() {
     //const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
-    const url = 'https://raw.githubusercontent.com/ricardoorrico/js-developer-portfolio/blob/main/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/ricardoorrico/projetofinal/refs/heads/main/data/profile.json'
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
